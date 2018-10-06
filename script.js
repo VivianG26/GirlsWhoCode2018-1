e56b3a6a4a44595d30ab265f61187a36575790bb
Loyola User <lmaliakal@luc.edu>
2018-10-06 11:50:46 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	
+	console.log("hello I'm here!!");
+	
+    $("p").click(function(){
+        $("p").hide();
+	});
+		
+		
+	$("img").click(function(){
+		$("p").show();
+    });
+});
